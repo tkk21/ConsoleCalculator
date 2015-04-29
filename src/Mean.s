3fdd304f7da3b8ff81a7604c1ad4993bb7497bf2
Ted Kim <tkk21@case.edu>
2015-04-29 12:58:40 -0400

    get rid of unnecessary lines

--- src/Mean.s
+++ src/Mean.s
@@ -2,18 +2,11 @@
 .globl mean
 mean:
 	addu	$s5, $0, $ra	#save the return address in a global register
-	
 	jal addition
-	
 	mtc1 $v0, $f0 #convert sum of arr into double
 	cvt.d.w $f0, $f0
-	
 	mtc1 $a1, $f2 #convert size of arr into double
 	cvt.d.w $f2, $f2
-	
 	div.d $f0, $f0, $f2 # sum/size
-	
-	
-	
 	addu	$ra, $0, $s5	#restore the return address
 	jr $ra
